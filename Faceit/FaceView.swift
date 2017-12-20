6358c6a19c9b077bef607913052fff0880b41f86
Unknown <markus.eson@hotmail.com>
2017-12-20 15:57:58 +0100

    new

---
+++ Faceit/FaceView.swift
@@ -0,0 +1,21 @@
+//
+//  FaceView.swift
+//  Faceit
+//
+//  Created by Markus Eriksson on 2017-12-20.
+//  Copyright © 2017 Markus Eriksson. All rights reserved.
+//
+
+import UIKit
+
+class FaceView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
